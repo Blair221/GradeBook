c9b2b1a7af57360347d3445deda4c945847de87b
Blair Brownlie <bbrownli@asu.edu>
2020-01-30 11:10:25 -0700

    getting familiar with certain built in properties and methods pertaining to maths

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,20 +9,29 @@ namespace GradeBook
         {
             var book = new Book("Blair's GradeBook");
             book.Addgrade(89.1);
-            
+            book.Addgrade(90.2);
+            book.Addgrade(76.4);
+
 
 
             List<double> grades = new List<double>() { 7.7, 3.3, 10.10 };
 
             var result = 0.0;
-            
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
             foreach (double number in grades)
             {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
                 result += number;
             }
             result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-            
+            Console.WriteLine($"The average grade is {result:N2}");
+            Console.WriteLine($"The highest grade is {highGrade:N2}");
+            Console.WriteLine($"The Lowest grade is {lowGrade:N2}");
+
 
         }
     }
