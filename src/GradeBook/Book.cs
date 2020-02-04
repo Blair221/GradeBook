07dc4f8cdd65b9cdbaea58e6ef06d51548e0bec2
Blair Brownlie <bbrownli@asu.edu>
2020-02-03 18:41:36 -0700

    starting to wrap my head around encapsulation as well as more keywords and access modifiers

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -12,6 +13,24 @@ namespace GradeBook
         public void Addgrade(double grade)
         {
             grades.Add(grade);
+        }
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N2}");
+            Console.WriteLine($"The highest grade is {highGrade:N2}");
+            Console.WriteLine($"The Lowest grade is {lowGrade:N2}");
         }
         private List<double> grades;
         private string name;
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -12,25 +12,9 @@ namespace GradeBook
             book.Addgrade(90.2);
             book.Addgrade(76.4);
 
+            book.ShowStats();
 
-
-            List<double> grades = new List<double>() { 7.7, 3.3, 10.10 };
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach (double number in grades)
-            {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-
-                result += number;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N2}");
-            Console.WriteLine($"The highest grade is {highGrade:N2}");
-            Console.WriteLine($"The Lowest grade is {lowGrade:N2}");
+            
 
 
         }
